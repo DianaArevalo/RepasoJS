6fcf75e7b98f25beb00d44e0edee3ad53139d736
DianaArevalo <morec9834@gmail.com>
2023-09-07 21:29:09 -0500

    de variables a eventos

---
+++ repaso javascript/script.js
@@ -0,0 +1,57 @@
+// tipos de dato
+
+// // booleano
+// let a = true
+// let b = false
+
+// // undefined
+// let c // no se asigna valor
+
+// //null
+// let d = null
+
+// // object
+
+// // let objeto = {
+// //     clave: valor
+// // }
+
+// // array }
+// let array = [1, 2, 3, 4, 5, 6]
+// let array2 = ['html', 'css', 'javascript']
+
+//date
+
+// const date = new Date("07-09-2023")
+
+// console.log(date);
+
+// const a = 5
+// const b = 9
+
+// function suma(x, y) {
+//     return x + y
+// }
+
+// const respuesta = suma(a, b)
+
+// console.log(`la respuesta de ${a} + ${b} es igual a:`, respuesta);
+
+
+// const temperatura = 76
+
+// function transformaraCelsius(fahrenheit) {
+//     return (fahrenheit - 32) * (5 / 9)
+// }
+
+// const res = transformaraCelsius(temperatura)
+// console.log(`la temperatura en grados Celsius es de `, parseInt(res));
+
+// function mostrar() {
+//     document.getElementById('seleccionable').innerHTML = Date()
+// }
+
+//strings
+
+
+
